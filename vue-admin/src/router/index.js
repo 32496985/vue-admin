f081d42e4fbf20188cb327ded718c13127180988
于元鑫 <32496985@qq.com>
2020-08-12 15:56:16 +0800

    项目构建、配置全局样式、路由、elementui

---
+++ vue-admin/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+// import Login from "../views/login"
+
+Vue.use(VueRouter);
+
+const routes = [{
+        path: "/",
+        redirect: "Login"
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: () =>
+            import ("../views/login")
+    },
+    {
+        path: "/about",
+        name: "About",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ "../views/About.vue")
+    }
+];
+
+const router = new VueRouter({
+    routes
+});
+
+export default router;
---
+++ vue-admin/src/views/login/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="login_bg">登录</div>
+</template>
+
+<script>
+export default {
+    name:'login'
+}
+</script>
+
+<style lang="scss" scoped>
+#login_bg{
+    height: 100vh;
+    background: #344a54;
+}
+</style>
